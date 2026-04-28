78ad1f141a6d2b118a3a6839e2e3699add39e940
awal1march <awal1march@gmail.com>
2026-04-28 12:45:29 +0000

    Fix server for production

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.get("/", (req, res) => {
   res.send("SUSU API running ✅");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log("Server running on " + PORT);
+  console.log(`Server running on port ${PORT}`);
 });
